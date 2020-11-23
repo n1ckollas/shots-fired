528316c76ceb451400ae5cb00e146352c98a4792
Mykola Rebezniuk <nick.rebez@gmail.com>
2020-11-22 21:47:59 -0500

    working socrata api.

--- backend/covid/settings.py
+++ backend/covid/settings.py
@@ -37,7 +37,16 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'home',
 ]
+
+REST_FRAMEWORK = {
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    'DEFAULT_PERMISSION_CLASSES': [
+    ]
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
--- backend/home/views.py
+++ backend/home/views.py
@@ -1,13 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.views import View 
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import authentication, permissions
 
 import requests
-# Create your views here.
-class Home(View):
-    greeting = "Hello World"
-    
+
+
+class Home(APIView):
+
     def get(self, request):
-        r = requests.get("https://jsonplaceholder.typicode.com/todos/1")
-        print(r.json())
-        return HttpResponse(self.greeting)
+        r = requests.get("https://data.cityofnewyork.us/resource/rc75-m7u3.json?date_of_interest=2020-11-19T00:00:00.000")
+        return Response(r.json())

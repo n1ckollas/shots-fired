73e6b27b92950b43049068cb6ceecc32f0ab728a
Mykola Rebezniuk <nick.rebez@gmail.com>
2020-11-30 20:55:50 -0500

    Navigation and Chart components.

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { PrimeNgModule } from './prime-ng.module';
+import { HttpClientModule } from '@angular/common/http';
+
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
-import { HttpClientModule } from '@angular/common/http';
+import { NaviComponent } from './components/navi/navi.component';
+import { ChartComponent } from './components/chart/chart.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    NaviComponent,
+    ChartComponent,
   ],
   imports: [
     BrowserModule,
--- frontend/src/app/prime-ng.module.ts
+++ frontend/src/app/prime-ng.module.ts
@@ -3,10 +3,12 @@ import { CommonModule } from '@angular/common';
 
 import { ButtonModule } from 'primeng/button';
 import { CardModule } from 'primeng/card';
+import { MenubarModule } from 'primeng/menubar';
 
 const modules = [
     ButtonModule,
     CardModule,
+    MenubarModule,
 ]
 
 @NgModule({

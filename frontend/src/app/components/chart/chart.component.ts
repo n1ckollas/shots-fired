5cfe83e1bdf092e2c5f181bdb1f203f8138a0c78
Mykola Rebezniuk <nick.rebez@gmail.com>
2020-12-05 21:45:24 -0500

    Themes for Chart.

--- frontend/src/app/components/chart/chart.component.ts
+++ frontend/src/app/components/chart/chart.component.ts
@@ -1,4 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from 'src/app/services/api.service';
+
+import * as Highcharts from 'highcharts';
+import Dark from 'highcharts/themes/high-contrast-dark'
+import Light from 'highcharts/themes/high-contrast-light'
+
+declare var require: any;
+let Boost = require('highcharts/modules/boost');
+let noData = require('highcharts/modules/no-data-to-display');
+let More = require('highcharts/highcharts-more');
+
+Boost(Highcharts);
+noData(Highcharts);
+More(Highcharts);
+noData(Highcharts)
+
 
 @Component({
   selector: 'app-chart',
@@ -6,10 +22,70 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./chart.component.scss']
 })
 export class ChartComponent implements OnInit {
+  public options: any = {
+      title: {
+          text: ''
+      },
 
-  constructor() { }
+      yAxis: {
+          title: {
+              text: 'Number of Deaths'
+          }
+      },
+
+      xAxis: {
+          categories: [],
+      },
+
+      legend: {
+          layout: 'horizontal',
+          align: 'center',
+          verticalAlign: 'bottom'
+      },
+
+      series: [],
+
+      responsive: {
+          rules: [{
+              condition: {
+                  maxWidth: 500
+              },
+              chartOptions: {
+                  legend: {
+                      layout: 'horizontal',
+                      align: 'center',
+                      verticalAlign: 'bottom'
+                  }
+              }
+          }]
+      }
+  };
+  months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+  chart: any;
+
+  constructor(private apiService: ApiService) { }
 
   ngOnInit(): void {
+    this.getData();
   }
 
+  getData(){
+    this.darkTheme();
+    this.apiService.getData().subscribe(data => {
+        this.options.series = data.series;
+        this.options.xAxis.categories = data.categories;       
+        this.chart = Highcharts.chart('chart-container', this.options);
+    });
+  }
+
+  darkTheme(): void{
+    Dark(Highcharts);
+  }
+
+  lightTheme():void {
+    Light(Highcharts);
+  }
+  reRenderChart(): void {
+    Highcharts.chart('chart-container', this.options);
+  }
 }
--- frontend/src/app/components/home/home.component.ts
+++ frontend/src/app/components/home/home.component.ts
@@ -1,19 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ApiService, IHomeScreenData } from 'src/app/services/api.service';
 
-import * as Highcharts from 'highcharts';
-import darkBlue from 'highcharts/themes/dark-blue'
-import darkUnica from 'highcharts/themes/high-contrast-dark'
-
-declare var require: any;
-let Boost = require('highcharts/modules/boost');
-let noData = require('highcharts/modules/no-data-to-display');
-let More = require('highcharts/highcharts-more');
-
-Boost(Highcharts);
-noData(Highcharts);
-More(Highcharts);
-noData(Highcharts)
 
 @Component({
   selector: 'app-home',
@@ -21,75 +7,9 @@ noData(Highcharts)
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+ 
+  constructor() { }
 
-    show = false;
-    public options: any = {
-        title: {
-            text: ''
-        },
-
-        yAxis: {
-            title: {
-                text: 'Number of Deaths'
-            }
-        },
-
-        xAxis: {
-            categories: [],
-        },
-
-        legend: {
-            layout: 'horizontal',
-            align: 'center',
-            verticalAlign: 'bottom'
-        },
-
-        series: [],
-
-        responsive: {
-            rules: [{
-                condition: {
-                    maxWidth: 500
-                },
-                chartOptions: {
-                    legend: {
-                        layout: 'horizontal',
-                        align: 'center',
-                        verticalAlign: 'bottom'
-                    }
-                }
-            }]
-        }
-    };
-
-  months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
-  chart: any;
-
-  
-  constructor(private apiService: ApiService) { }
-
-  ngOnInit(){
-      this.getData();
-  }
-
-  getData(){
-    // darkBlue(Highcharts);
-    darkUnica(Highcharts);
-    this.apiService.getData().subscribe(data => {
-        this.options.series = data.series;
-        this.options.xAxis.categories = data.categories;       
-        this.chart = Highcharts.chart('chart-container', this.options, (value) => {
-            console.log(value);
-            console.log('finished');
-            this.show = true;
-        });
-    });
-  }
-
-  update(){
-    this.options.title.text = "NEW TITLE"
-    darkBlue(Highcharts);
-    Highcharts.chart('chart-container', this.options);
-  }
+  ngOnInit(): void { }
 
 }

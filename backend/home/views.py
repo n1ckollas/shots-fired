c95af0ec6e3a5fce7d96896e50ca4d8dd50c5928
Mykola Rebezniuk <nick.rebez@gmail.com>
2020-12-06 20:51:07 -0500

    Clean up for the data processing.

--- backend/home/views.py
+++ backend/home/views.py
@@ -46,33 +46,12 @@ class DeathCountPerBorough(APIView):
     def get(self, request):
         r = requests.get("https://data.cityofnewyork.us/resource/rc75-m7u3.json?$order=date_of_interest")
         data = r.json()
-        categories = []
         series = []
-
-        bk = { 'name': 'Brooklyn',       'data': [] }
-        bx = { 'name': 'Bronx',          'data': [] }
-        mn = { 'name': 'Manhattan',      'data': [] }
-        qn = { 'name': 'Queens',         'data': [] }
-        si = { 'name': 'Staten Island',  'data': [] }
         ep = datetime(1970, 1, 1, 0, 0)
+        
         for d in data:
             date = datetime.strptime(d['date_of_interest'], "%Y-%m-%dT%H:%M:%S.%f")
             x = (date - ep).total_seconds() * 1000;
-            # categories.append(date.strftime("%b %d %Y")) 
-
-            # bk['data'].append(int(d['bk_death_count']))
-            # bx['data'].append(int(d['bx_death_count']))
-            # mn['data'].append(int(d['mn_death_count']))
-            # qn['data'].append(int(d['qn_death_count']))
-            # si['data'].append(int(d['si_death_count']))
             series.append([x, int(d['bk_death_count'])])
-
-        # series.append(bk)
-        # series.append(bx)
-        # series.append(mn)
-        # series.append(qn)
-        # series.append(si)
-
-        data_set = {"categories": categories, "series":series}
 
         return Response(series)
